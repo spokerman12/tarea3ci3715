e7ad3dba56d87fc51656a53a8c5bfca9e6d54f1d
Leonardo Lopez <14-10576@usb.ve>
2018-05-16 16:56:45 -0400

    Refactorización y comentarios de la clase seguridad.

--- seguridad.py
+++ seguridad.py
@@ -18,26 +18,36 @@ import re
 class Seguridad():
 
     def __init__(self):
+        # Crea un diccionario vacio.
         self.usuarios = {}
-
+        
+    """
+    Recibe 3 strings una de usuario y 2 de clave.
+    Realiza las comprobaciones pertinentes y registra al usuario.
+    Retorna True si se registro correctamente al usuario.
+    Retorna False en caso contrario.
+    """
     def registrarUsuario(self, usuario, clave1, clave2):
-        #assert(not(usuario in self.usuarios.keys()))
-
+        # Comprobamos si el email sigue el estandar .
         if not (re.match('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',usuario)):
             print("Correo electronico invalido. No sigue el formato")
             return False
-
+        
+        # Comprobamos que la clave tenga la longitud deseada.
         if not(8 <= len(clave1) <= 16) :
             print("Clave invalida. Longitud debe estar entre 8 y 16 caracteres")
             return False
 
+        # Comprobamos que la clave solo contenga caracteres alfanumericos (no especiales).
         if not(re.match('[^\W_]',clave1)):
             print("Clave invalida. Contiene caracteres especiales")
             return False
 
-        m = 0
-        M = 0
-        d = False
+        # Comprobamos que la clave tenga la cantidad necesaria de mayusculas, minusculas y digitos.
+        m = 0 # contador de minusculas .
+        M = 0 # contador de mayusculas.
+        d = False # Se le asigna true si se encuentra un digito.
+        # iteramos por cada caracter de la clave.
         for a in list(clave1) :
             if a.isupper() :
                 M+=1
@@ -46,25 +56,37 @@ class Seguridad():
             if a.isdigit():
                 d = True
 
+        # Comprobamos que la clave cumpla con las restricciones.
         if M == 0 or m == 0 or m+M < 3 or d == False:
             print("Clave invalida. Debe incluir un numero y al menos 3 letras, una mayuscula y una minuscula")
             return False
 
+        # Comprobamos que el email aparezca en el diccionario.
         if usuario in self.usuarios.keys():
             print("Correo electronico invalido. Ya esta registrado")
             return False
+        
+        # Comprobamos que las claves suministradas sean iguales.
         if clave1 != clave2:
             print("Clave invalida. Las claves no coinciden")
             return False
-        if clave1 == "":
-            print("Clave invalida. No puede ser vacia")
-            return False
+        
+        # En caso 
+        # Se guarda en el diccionario un par con el correo y la clave invertida
         self.usuarios[usuario] = clave1[::-1]
         return True
 
+    """
+    Recibe 2 strings una de usuario y una de clave.
+    Retorna True si el usuario existe en el 
+    diccionario y la clave es correcta.
+    Retorna False en caso contrario.
+    Imprime por la salida estandar un mensaje segun el caso.
+    """
     def ingresarUsuario(self, usuario, clave) :
-        print(self.usuarios)
+        # Comprobamos que el usuario aparezca en el diccionario.
         if usuario in self.usuarios.keys():
+            # Comprobamos que la clave coincida con la guardada en el diccionario.
             if self.usuarios[usuario] == clave[::-1]:
                 print("Usuario aceptado")
                 return True
